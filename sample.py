04420fba73c8609446e116255e5639c1dcd8717c
Akshat Singh <akshatsingh27433@gmail.com>
2025-05-02 01:56:44 +0530

    test commit

--- sample.py
+++ sample.py
@@ -1,26 +1,26 @@
-import os, sys  # Multiple imports on the same line (not recommended)
+import os, sys  # Multiple imports on a single line
 
 def example_function():
-    print("This is an example")  # IndentationError
+  print("This is an example function")  # Indentation error: This line should be indented
 
-def unused_function():  # Unused function (Pylint will detect this)
-    pass
+def unused_function():  # Unused function will be flagged
+  pass
 
 def divide_numbers(a, b):
-    result = a / b  # Potential ZeroDivisionError
-    return result
-    
-class myClass:  # Class name should follow PascalCase (PEP8)
-    def __init__(self, value):
-        self.value = value
-    
-    def get_value(self):
-        return self.value
+  result = a / b  # Potential ZeroDivisionError when b is zero
+  return result
+
+class myClass:  # Class name doesn't follow PEP8 (should be MyClass)
+  def __init__(self, value):
+    self.value = value
+  
+  def get_value(self):
+    return self.value
 
 def security_risk():
-    user_input = input("Enter command: ")
-    os.system(user_input)  # Security Risk: Arbitrary command execution
+  user_input = input("Enter a command: ")
+  os.system(user_input)  # Security risk: This could execute harmful commands
 
-x = { 'key' : "value" }  # Incorrect spacing in dictionary
+x = { 'key' : "value" }  # Incorrect dictionary spacing, should be {'key': 'value'}
 
-print( "Hello, World!" )  # Unnecessary spaces inside parentheses
+print( "Hello, World!" )  # Unnecessary spaces around the parentheses
