5302cbcb0da7b108e7e6548c44c60e49cba55de3
Akshat Singh <akshatsingh27433@gmail.com>
2025-05-02 13:48:03 +0530

    Sample commit

--- sample.py
+++ sample.py
@@ -52,4 +52,4 @@ if __name__ == "__main__":
     file_content = read_file_content("example.txt")
     if file_content:
         print(f"Content of example.txt: {file_content[:50]}...")  # Print only first 50 chars
-    print("End of program") #Added a print statement
+    print("End of program")  # Added a print statement
